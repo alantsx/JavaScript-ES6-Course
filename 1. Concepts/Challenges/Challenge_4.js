c105fb2f8a536c2da675b279fe6f4f41e78bbe8f
Alan Pereira <alanpfabricio@gmail.com>
2020-05-30 22:14:17 -0300

    Add files via upload

---
+++ 1. Concepts/Challenges/Challenge_4.js
@@ -0,0 +1,20 @@
+const empresa = {
+    nome: 'Rocketseat',
+    endereco: {
+    cidade: 'Rio do Sul',
+    estado: 'SC',
+    }
+};
+
+const { nome, endereco: { cidade }, endereco: { estado } } = empresa;
+
+console.log (nome);
+console.log (cidade);
+console.log (estado);
+
+
+function mostraInfo(usuario) {
+    const { nome, idade } = usuario;
+    return `${nome} tem ${idade} anos.`;
+    }
+console.log(mostraInfo({ nome: 'Diego', idade: 23 }));
